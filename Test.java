b0cc3b7b2d6fc5fc551ac0be4a66c7599b7b1a4b
Bisliu <160489818+Bisliu@users.noreply.github.com>
2024-05-11 23:30:40 +0800

    Add files via upload

--- Test.java
+++ Test.java
@@ -1,31 +1,11 @@
-//import java.util.Arrays;
-//class Solution {
-//    public double trimMean(int[] arr) {
-//        Arrays.sort(arr);
-//        double a=0;
-//        double sum=0;
-//        for(int i=(int)(0.05*arr.length);i<0.95*arr.length;i++){
-//            sum+=arr[i];
-//        }
-//        a=sum/(arr.length*0.9);
-//        return a;
-//    }
-//}
-//
-//import java.util.Arrays;
-//class Solution {
-//    public long maximumHappinessSum(int[] happiness, int k) {
-//        Arrays.sort(happiness);
-//        long sum=0;
-//        for(int i=happiness.length-1;i>happiness.length-1-k;i--){
-//            sum+=happiness[i];
-//            for(int a=0;a<i;a++){
-//                if(happiness[a]==0){
-//                    continue;
-//                }
-//                happiness[a]--;
-//            }
-//        }
-//        return sum;
-//    }
-//}
+class Solution {
+    public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
+        int count=0;
+        for(int i=0;i<hours.length;i++){
+            if(hours[i]>=target){
+                count++;
+            }
+        }
+        return count;
+    }
+}
