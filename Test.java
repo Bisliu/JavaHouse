64e76ae56102f147745c50204ec719641763d126
Bisliu <160489818+Bisliu@users.noreply.github.com>
2024-05-12 21:07:42 +0800

    Add files via upload

--- Test.java
+++ Test.java
@@ -1,11 +1,12 @@
 class Solution {
-    public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
-        int count=0;
-        for(int i=0;i<hours.length;i++){
-            if(hours[i]>=target){
-                count++;
-            }
+    public int kthGrammar(int n, int k) {
+        if (n == 1) return 0;
+
+        int half = 1<<(n-2);
+        if(k>half){
+            return kthGrammar(n - 1, k - half) ^ 1;
         }
-        return count;
+        return kthGrammar(n - 1, k);
     }
 }
+
